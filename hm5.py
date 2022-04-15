80a69fa77137ad25d287a4d562339fdccd24ad0c
roflmorries <leynwork172@gmail.com>
2022-04-16 01:06:16 +0300

    Homework5

---
+++ hm5.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+ALL_USERS = [
+    {
+        'username': 'Matthew',
+        'age': '18',
+        'id': '1'
+
+    },
+    {
+        'username': 'Jane',
+        'age': '20',
+        'id': '2'
+    },
+    {
+        'username': 'Wayne',
+        'age': '25',
+        'id': '3'
+    },
+    {
+        'username': 'Kenzie',
+        'age': '27',
+        'id': '4'
+    }
+]
+
+
+@app.route('/user')
+def users():
+    return jsonify(ALL_USERS)
+
+
+@app.route('/user/<int:id>')
+def get_id(id):
+    for user in range(len(ALL_USERS)):
+        if ALL_USERS[user]['id'] == id:
+            return jsonify(ALL_USERS[id - 1]['username'], ALL_USERS[id - 1]['age'])
+        else:
+            return 'Пользователя с таким ID не существует!'
+
+
+app.run('localhost', 8000)
