9ad063902dc52d390b19a328ef81e95bb55421ab
roflmorries <99510874+roflmorries@users.noreply.github.com>
2022-05-21 12:56:25 +0300

    Update hm9.py

--- hm9.py
+++ hm9.py
@@ -1,23 +1,34 @@
 @article_api.put(config.API_ROUTE_PREFIX + 'articles/<id>')
-def create_article(id):
+def update_article(id):
 	if 'title' not in request.json or 'content' not in request.json or 'author' not in request.json:
 		return flask.jsonify({
 				'code': 9,
 				'message': 'Title, content, author являются обязательными полями',
 				'data': None
 			})
-	request.json['update_at'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	data = article_update.crud(article.dict(), id)
+	if data is None:
+		return flask.jsonify({
+			'code': 9,
+			'message': 'Статья не найдена!',
+			'data': None
 	return flask.jsonify({
 			'code': 0,
 			'message': 'OK',
-			'data': article_crud.update(id - 1)
+			'data': None
 		})
 
 @article_api.delete(config.API_ROUTE_PREFIX + 'articles/<id>')
-def del_article(id):
+def delete_article(id):
+	data = article_delete.crud(article.dict(), id)
+	if data is None:
+		return flask.jsonify({
+			'code': 9,
+			'message': 'Статья не найдена!',
+			'data': None
 	return flask.jsonify({
 			'code': 0,
 			'message': 'OK',
-			'data': article_crud.delete(id - 1)
+			'data': None
 		})
 
