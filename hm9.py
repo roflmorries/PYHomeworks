5855464b1ac36aa50145c78e031e54ef85a9ed93
roflmorries <99510874+roflmorries@users.noreply.github.com>
2022-06-07 02:13:02 +0300

    Update hm9.py

--- hm9.py
+++ hm9.py
@@ -1,17 +1,16 @@
 @article_api.put(config.API_ROUTE_PREFIX + 'articles/<id>')
 def update_article(id):
-	if 'title' not in request.json or 'content' not in request.json or 'author' not in request.json:
-		return flask.jsonify({
-				'code': 9,
-				'message': 'Title, content, author являются обязательными полями',
-				'data': None
-			})
-	data = article_update.crud(article.dict(), id)
+	try:
+		article = Article(id=str(id), title=request.json.get('title'), content=request.json.get('content'), author=request.json.get('author'))
+	except ValidationError as e:  # Отловили ошибку PyDantic и записали ее в переменную e
+		return e.json()  # Преобразовали ошибку в JSON и вернули ее
+	data = article_crud.update(article.dict(), id)
 	if data is None:
 		return flask.jsonify({
-			'code': 9,
-			'message': 'Статья не найдена!',
-			'data': None
+				'code': 9,
+				'message': 'Статья не найдена',
+				'data': None
+			})
 	return flask.jsonify({
 			'code': 0,
 			'message': 'OK',
