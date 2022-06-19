86f0c63e7775e3ec3ed25bcf47b8b2879de5f680
roflmorries <99510874+roflmorries@users.noreply.github.com>
2022-06-20 01:11:05 +0300

    Update models.py

--- hm11/hillel-django-master/task/models.py
+++ hm11/hillel-django-master/task/models.py
@@ -27,5 +27,5 @@ class Task(models.Model):
 class Comment(models.Model):
 	id = models.AutoField(primary_key=True)
 	text = models.TextField()
-	updated_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 	created_at = models.DateTimeField(auto_now_add=True)
